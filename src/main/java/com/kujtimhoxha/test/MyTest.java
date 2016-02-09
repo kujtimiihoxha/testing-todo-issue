6ac9fbd2e2d1d97790267c006f99ae59f071b78d
kujtimiihoxha <kujtimii.h@gmail.com>
2016-02-09 18:09:48 +0100

    Initial Commit

---
+++ src/main/java/com/kujtimhoxha/test/MyTest.java
@@ -0,0 +1,19 @@
+package com.kujtimhoxha.test;
+
+/**
+ * MyTest.
+ *
+ * @author Kujtim Hoxha (kujtimii.h@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class MyTest {
+    /**
+     *
+     */
+    private static final String myString="Testing todo-issue";
+
+    public static void myMethod(){
+        System.out.println("MyTest.myString = " + MyTest.myString);
+    }
+}
